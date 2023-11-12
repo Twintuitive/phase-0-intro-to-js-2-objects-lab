436bc524d66444518a5b9e394738fa9eecad1f3e
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-12 17:14:31 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St."
+}
+// Define the function updateEmployeeWithKeyAndValue
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+  // Define the function destructivelyUpdateEmployeeWithKeyAndValue
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+  }
+  
+  // Define the function destructivelyDeleteFromEmployeeByKey
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+  }
+  
+ // Define the function destructivelyDeleteFromEmployeeByKey
+ function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee
+  }
